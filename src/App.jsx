d79c471966840a86114ad442f68b8c759f21253e
Ogasawara <kakeru19870423@gmail.com>
2026-06-05 12:53:08 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import Hero from './components/Hero'
+import About from './components/about'
+import Navbar from './components/Navbar'
+import Features from './components/Features'
+import Story from './components/story'
+import Contact from './components/Contact'
+import Footer from './components/Footer'
+
+const App = () => {
+  return (
+    <main className='relative min-h-screen w-screen overflow-x-hidden'>
+      <Navbar />
+      <Hero />
+      <About />
+      <Features />
+      <Story />
+      <Contact />
+      <Footer />
+    </main>
+  )
+}
+
+export default App
