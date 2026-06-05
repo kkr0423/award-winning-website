ec562a859513af105bcb66405d4d155ac81b5cc7
Ogasawara <kakeru19870423@gmail.com>
2026-06-05 12:59:49 +0900

    fix import

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Hero from './components/Hero'
 import About from './components/About'
 import Navbar from './components/Navbar'
 import Features from './components/Features'
-import Story from './components/story'
+import Story from './components/Story'
 import Contact from './components/Contact'
 import Footer from './components/Footer'
 
